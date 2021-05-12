86e511a55809ae9e641f746685cf8305ed6bf827
Connor <71216273+cm-watson@users.noreply.github.com>
2021-05-12 21:08:17 +1000

    Add Publications and Authors

---
+++ Assignment 2/Assignment 2/Publications.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    class Publications
+    {
+        string doi;
+        string title;
+        List<authors> authors;
+        int year;
+        type type;
+        string cite;
+        int date_available;
+        int age;
+
+        // string DOI {
+        //     get { return doi; }
+        //     set { doi = value; }
+        // }
+
+
+
+        public Publications(string doi, string title, int year, string cite, int date_available, int age)
+        {
+            this.doi = doi;
+            this.title = title;
+            this.year = year;
+            this.cite = cite;
+            this.date_available = date_available;
+            this.age = age;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Publications publication = new Publications("idk", "title", 2021, "idk", 21, 21);
+
+            Console.WriteLine(publication);
+
+        }
+
+        static string formatYear(int year, string title)
+        {
+
+        }
+
+        static string sortAlphabetically(string title)
+        {
+
+        }
+    }
+}
