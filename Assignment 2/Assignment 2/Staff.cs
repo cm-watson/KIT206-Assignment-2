530f1492e04b31947e4dffff5d6794138197af16
joooood <83745317+joooood@users.noreply.github.com>
2021-05-12 15:57:31 +1000

    created the staff.cs starting point

---
+++ Assignment 2/Assignment 2/Staff.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    class Staff
+    {
+        public float ThreeYearAverage()
+        {
+            return;
+        }
+        public float Performance()
+        {
+            return;
+        }
+    }
+}
