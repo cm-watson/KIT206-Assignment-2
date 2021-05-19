00db627015b21758f16ff0307a5a519451beea3e
joooood <83745317+joooood@users.noreply.github.com>
2021-05-19 14:15:45 +1000

    changed Publication.cs

--- Assignment 2/Assignment 2/Publication.cs
+++ Assignment 2/Assignment 2/Publication.cs
@@ -8,14 +8,14 @@ namespace Assignment_2
 {
     class Publication
     {
-        string doi;
-        string title;
-        List<Author> authors;
-        int year;
-        Type type;
-        string cite;
-        int date_available;
-        int age;
+        public string DOI { get; set; }
+        public string Title { get; set; }
+        public List<Author> Authors { get; set; }
+        public int Year { get; set; }
+        public Type OutputType { get; set; }
+        public string Cite { get; set; }
+        public int DateAvailable { get; set; }
+        public int Age { get; set; }
 
         // string DOI {
         //     get { return doi; }
@@ -26,12 +26,12 @@ namespace Assignment_2
 
         public Publication(string doi, string title, int year, string cite, int date_available, int age)
         {
-            this.doi = doi;
-            this.title = title;
-            this.year = year;
-            this.cite = cite;
-            this.date_available = date_available;
-            this.age = age;
+            this.DOI = doi;
+            this.Title = title;
+            this.Year = year;
+            this.Cite = cite;
+            this.DateAvailable = date_available;
+            this.Age = age;
         }
 
         static void Main(string[] args)
