32b0ffb46dea9b0bd6f5aabe8e0f7dca10631baa
joooood <83745317+joooood@users.noreply.github.com>
2021-05-20 21:17:28 +1000

    added comments to Publication.cs

--- Assignment 2/Assignment 2/Publication.cs
+++ Assignment 2/Assignment 2/Publication.cs
@@ -6,25 +6,29 @@ using System.Threading.Tasks;
 
 namespace Assignment_2
 {
+    // The type of publication the Publication is
     public enum OutputType { Conference, Journal, Other };
+
     public class Publication
     {
+        // A digital object identifier for the Publication
         public string DOI { get; set; }
+        // The title of the Publication
         public string Title { get; set; }
+        // A list of authors that contributed to the Publication
         public List<string> Authors { get; set; }
+        // The year the Publication was published
         public int Year { get; set; }
+        // The type of publication the Publication is
         public Type OutputType { get; set; }
+        // A string containing what the Publication should be cited as
         public string Cite { get; set; }
+        // The year the Publication first became available
         public int DateAvailable { get; set; }
+        // The time (in days elapsed) since the Publication became available
         public int Age { get; set; }
 
-        // string DOI {
-        //     get { return doi; }
-        //     set { doi = value; }
-        // }
-
-
-
+        // Publication Constructor
         public Publication(string doi, string title, int year, string cite, int date_available, int age)
         {
             this.DOI = doi;
@@ -35,6 +39,8 @@ namespace Assignment_2
             this.Age = age;
         }
 
+        /* For testing
+         *
         static void Main(string[] args)
         {
 
@@ -43,12 +49,15 @@ namespace Assignment_2
             Console.WriteLine(publication);
 
         }
+        */
 
+        // Formats the year?
         static string FormatYear(int year, string title)
         {
 
         }
 
+        // Sorts the Publications alphabetically
         static string SortAlphabetically(string title)
         {
 
