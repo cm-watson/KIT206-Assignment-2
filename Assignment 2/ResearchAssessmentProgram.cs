a423328459ccf405b6ddd1f9c9f4528c44d4ca4c
Not Fat Xavier <83687638+LastMinuteClub@users.noreply.github.com>
2021-05-24 16:26:31 +1000

    Update ResearchAssessmentProgram.cs

---
+++ Assignment 2/ResearchAssessmentProgram.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    class ResearchAssessmentProgram
+    {
+        static void Main(string[] args)
+        {
+        	Console.Write("Test");
+        }  
+    }
+}
