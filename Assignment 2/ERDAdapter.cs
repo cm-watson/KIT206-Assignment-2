2742b037757092b64ff17d82603db110b9ba6d4f
joooood <83745317+joooood@users.noreply.github.com>
2021-05-25 23:04:58 +1000

    asfas

--- Assignment 2/ERDAdapter.cs
+++ Assignment 2/ERDAdapter.cs
@@ -140,7 +140,7 @@ namespace Assignment_2
                         "",                                 // Email
                         rdr.GetString(6),                   // Photo
                         "",                                 // Degree
-                        0,                                  // Superviser ID
+                        0,                                  // Supervisor ID
                         new List<Publication>(), 			// Publications
                         new List<Position> { 				// Positions
                         	new Position(ParseEnum<EmploymentLevel>(rdr.GetString(5)), rdr.GetDateTime(7), DateTime.MinValue) 
